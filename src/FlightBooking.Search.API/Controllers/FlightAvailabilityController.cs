68bac559e738825bf0545f65022c0c6eb062dee7
Basharat Hussain <basharat58@hotmail.com>
2020-04-16 14:30:48 +0100

    Setup the Inital projects.
    Added the inital projects.

---
+++ src/FlightBooking.Search.API/Controllers/FlightAvailabilityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Search.API.Controllers
+{
+    [Route("api/flightavailability")]
+    [ApiController]
+    public class FlightAvailabilityController : ControllerBase
+    {
+
+    }
+}
